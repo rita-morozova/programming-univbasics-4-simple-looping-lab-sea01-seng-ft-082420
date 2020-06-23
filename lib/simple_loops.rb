07f9f67a2248ff8a5552afcb147224ce41f8c125
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-23 16:16:18 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,12 +21,7 @@ def output_array (array)
 end 
 
 def return_string_array (array)
-   counter = 0 
-   while array [counter] do 
-     puts new_array = array[counter].to_s
-     counter +=1 
-   end 
-   new_array
+   new_array = array.map
  end
     
     
