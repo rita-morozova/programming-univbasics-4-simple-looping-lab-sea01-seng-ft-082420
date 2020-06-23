f2f6ca77349310bef9c4be00a325188e1d900a56
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-23 15:05:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,8 @@
 # Write your methods here
-def loop_message_five_times (string)
+def loop_message_five_times (message)
   counter = 0
-  while string.length < 4
-  puts string
+  while message.length < 4
+  puts message
   counter +=1 
 end
 end
