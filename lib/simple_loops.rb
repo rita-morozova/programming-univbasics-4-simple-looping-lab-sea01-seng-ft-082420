306283de096b6b2a109748c5c8f8e2f342a8f3de
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-23 15:59:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,7 +23,7 @@ end
 def return_string_array (array)
    counter = 0 
    while array [counter] do 
-     puts new_array = array[counter].to_s.push 
+     puts new_array = array[counter].to_s.push ()
      counter +=1 
    end 
    new_array
