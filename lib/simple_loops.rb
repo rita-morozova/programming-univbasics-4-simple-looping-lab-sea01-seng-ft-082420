135fa3dc8a582f6a79bb9c3506fdfa0e1228f5df
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-23 16:37:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,8 +21,10 @@ def output_array (array)
 end 
 
 def return_string_array (array)
-     new_array = array.each do |element|
-      #{element}.join (", ")
+     count = 0 
+     while count < array.length do 
+       puts new_array = array[count].to_s 
+       count +=1 
    end 
    new_array
  end
